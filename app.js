99838c3cbcd929e44cf7f66d1b72f5563dcbb9c7
smith@ariveguru.com <smith@ariveguru.com>
2020-06-08 09:12:57 +0530

    adding bodyparser for requests and handling cors

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 const productRoutes = require('./api/routes/poducts');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin',
+		'X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req === 'OPTIONS') {
+		res.header(
+			'Access-Control-Allow-Headers',
+			'PUT',
+			'POST',
+			'PATCH',
+			'DELETE',
+			'GET'
+		);
+		res.status(200).json({});
+	}
+	next();
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
